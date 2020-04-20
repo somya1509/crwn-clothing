ab52b748b1238f0d34d25a755043dff6c4216b15
Somya Singh <singh.somya1509@gmail.com>
2020-04-21 00:29:30 +0530

    Update our menu items with  Router HOC

--- src/components/menu-items/menu-items.component.jsx
+++ src/components/menu-items/menu-items.component.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 import './menu-items.styles.scss';
-const MenuItem = ({title, imageUrl, size}) => (
-   <div className= {`${size} menu-item`} >
+
+const MenuItem = ({title, imageUrl, size, history,linkUrl, match}) => (
+   <div className= {`${size} menu-item`} 
+        onClick= { () => history.push(`${match.url}${linkUrl}`)} >
+   
    <div className='background-image' 
    style ={{
         backgroundImage : `url(${imageUrl})`
     }} />
+
+
                 <div className='content'>
                     <h1 className='title'>{title.toUpperCase()}</h1>
                     <span className='subtitle'>Shop Now</span>
@@ -15,4 +21,4 @@ const MenuItem = ({title, imageUrl, size}) => (
            
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
